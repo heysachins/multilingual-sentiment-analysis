06d9784049b668006fa451884b8e656aa9ff99a7
heysachins <heysachins@gmail.com>
2024-02-05 15:30:45 +0000

    adding visualisations

--- LRtry1.py
+++ LRtry1.py
@@ -1,22 +1,33 @@
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, accuracy_score
+import pandas as pd # Imported to enable the use of datastructures like dataframe
+from sklearn.feature_extraction.text import TfidfVectorizer # Imported to convert raw documents into a matrix of tf idf features
+from sklearn.linear_model import LogisticRegression # Imported to enable the use of logistic regression to classify text
+from sklearn.model_selection import train_test_split # Imported to enable the user to split the data into train, test samples
+from sklearn.metrics import classification_report, accuracy_score # Imported to calculate the accuracy and also print the classification report
 
-# Load your dataset
-# Assuming the dataset has columns 'text' for the review and 'sentiment' for the sentiment label
+# Importing the dataset
 df = pd.read_csv('malayalam_train.tsv', sep='\t')
 print(df)
+
 # Preprocess the data (if necessary)
 
-# Splitting dataset into training and testing
-X_train, X_test, y_train, y_test = train_test_split(df['text'], df['category'], test_size=0.2, random_state=42)
+
+# Exploring the dataset
+print(df.shape())
+df.head()
+
+
+
+# Splitting the dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    df.text, 
+    df.category,
+    test_size=0.2, # 20 % of samples will be present in test dataset
+    random_state=42)
 
 # Vectorization using TF-IDF
-vectorizer = TfidfVectorizer()
-X_train_tfidf = vectorizer.fit_transform(X_train)
-X_test_tfidf = vectorizer.transform(X_test)
+v = TfidfVectorizer()   
+X_train_tfidf = v.fit_transform(X_train)
+X_test_tfidf = v.transform(X_test)
 
 # Logistic Regression model
 log_reg = LogisticRegression()
